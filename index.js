b46d1f7619387c6ad5295430a178115af9cb1ed4
Adler Oliveira <aoliveira@jooycar.cl>
2017-03-29 22:36:20 -0300

    ..

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const bodyParser = require('body-parser')
 const app = express()
 
 // Config
-const port = 8001
+const port = process.env.PORT || 8001
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 // Routes
-app.get('/', (_, res) => res.send('hello world'))
+app.get('/', (_request, response) => response.send('hello world'))
+
+app.get('/users', (_request, response) => {
+  response.send([])
+})
+
+app.post('/users', (_request, response) => {
+  response.send("ok")
+})
 
 // Actions
 app.listen(port, () => console.log(`Hex API - Server Listening on port ${port}`))
