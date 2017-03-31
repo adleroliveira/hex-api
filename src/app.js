2cb97dc7538e0401eb926e6b72b444280bfd9243
Adler Oliveira <aoliveira@jooycar.cl>
2017-03-30 23:44:56 -0300

    auth and routes

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// Libs
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const routes = require('./routes/v1')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const config = require('./config')
+const User   = require('./models/user')
+
+// Config
+const port = process.env.PORT || 8001
+const app = express()
+mongoose.connect(config.database)
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use("/api/v1", routes)
+app.set('jwtSecret', config.secret)
+
+// Actions
+app.listen(port, () => console.log(`Hex API - Server Listening on port ${port}`))
---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'hex',
+  'database': 'mongodb://localhost:27017/hex'
+}
